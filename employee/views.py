9b994432cb64633b92dd19609f2961df9393cf1b
tosswi <Heckel3437@gmail.com>
2020-06-06 02:45:55 +0900

    [add] search pagenate

--- employee/views.py
+++ employee/views.py
@@ -5,18 +5,27 @@ from .models import Employee
 
 class IndexView (generic.ListView):
   model = Employee
+  paginate_by = 1
 
-# def get_context_data(self):
-#   """テンプレートを渡す辞書の作成"""
-#   context = super().get_context_data()
-#   context['form'] = SearchForm(self.request.GET)
-#   return context
-#
-# def get_queryet(self)
-#   """テンプレートえ渡す"""
-#   form = SearchForm(self.request.GET)
-#   form.is_valid()
+  def get_context_data(self):
+    """テンプレートを渡す辞書の作成"""
+    context = super().get_context_data()
+    context['form'] = SearchForm(self.request.GET)
+    return context
 
+  def get_queryset(self):
+    """テンプレートえ渡す"""
+    form = SearchForm(self.request.GET)
+    form.is_valid()
+    queryset = super().get_queryset()
+    department =form.cleaned_data['department']
+    if department:
+      queryset=queryset.filter(department = department)
+
+    club = form.cleaned_data['club']
+    if club:
+      queryset=queryset.filter(club = club)
+    return queryset
 
 
 # Create your views here.
